8ba2962d87668ce784fc52dcdd3744b0b32b2c25
Charles-P. Clermont <charles@cpclermont.com>
2020-10-20 14:49:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Faster
+
+A Cloudflare worker that makes pages faster.
+
+Useful for WebPageTest hypothesis testing.
+
+See [fast.cpclermont.workers.dev](https://fast.cpclermont.workers.dev) for docs.
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,139 @@
+/**
+ * Configurable WebPageTest Cloudflare Worker.
+ *
+ * Options (sent as headers)
+ *  - `x-no-async-hide: true    `: disables Google Optimize anti-flicker snippet
+ *  - `x-async: <cssSelector>   `: add `async="true"` to nodes targeted by css selector
+ *  - `x-bypass-transform: true `: disable all transformations
+ *  - `x-defer: <cssSelector>   `: add `defer="true"` to nodes targeted by css selector
+ *  - `x-link: <linkHeader>     `: preconnect & preloads you'd want to add to the response
+ *  - `x-push: true             `: enables header streaming while we wait for the response
+ */
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function getResponse(request, shouldPush) {
+  // If push of headers is not enabled, return a copy of the response
+  // (so we can mutate the headers).
+  if (!shouldPush) {
+    const response = await fetch(request);
+    return new Response(response.body, response);
+  }
+
+  // Allow for pushing HTTP headers while we wait for the response
+  const { readable, writable } = new TransformStream()
+
+  fetch(request).then((response) => {
+    response.body.pipeTo(writable)
+  })
+
+  const allHeaders = [
+    ['connection', 'keep-alive'],
+    ['content-language', 'en'],
+    [
+      'content-security-policy',
+      "block-all-mixed-content; frame-ancestors 'none'; upgrade-insecure-requests;",
+    ],
+    ['content-type', 'text/html; charset=utf-8'],
+    ['server', 'cloudflare'],
+    ['strict-transport-security', 'max-age=7889238'],
+    ['vary', 'Accept'],
+    ['x-frame-options', 'DENY'],
+    ['x-permitted-cross-domain-policies', 'none'],
+    ['x-xss-protection', '1; mode=block'],
+  ]
+
+  const headers = new Headers()
+
+  for (const [k, v] of allHeaders) {
+    headers.append(k, v)
+  }
+
+  return new Response(readable, {
+    status: 200,
+    statusText: 'OK',
+    headers: headers,
+  });
+}
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  // Disallow crawlers
+  if (url.pathname === '/robots.txt') {
+    return new Response('User-agent: *\nDisallow: /', { status: 200 })
+  }
+
+  // When overrideHost is used in a script, WPT sets x-host to original host i.e. site we want to proxy
+  const host = request.headers.get('x-host')
+
+  // Error if x-host header missing
+  if (!host) {
+    return new Response('x-host header missing', { status: 403 })
+  }
+
+  console.log('host', host);
+  url.hostname = host
+
+  const bypassTransform = request.headers.get('x-bypass-transform')
+  const acceptHeader = request.headers.get('accept')
+
+  const isHtmlRequest = acceptHeader && acceptHeader.includes('text/html')
+  const shouldBypassTransform =
+    bypassTransform && bypassTransform.indexOf('true') !== -1
+  const shouldTransform = isHtmlRequest && !shouldBypassTransform
+
+  const req = new Request(url, request)
+  req.headers.delete('x-host')
+  req.headers.delete('x-forwarded-proto')
+  req.headers.delete('cf-visitor')
+  req.headers.delete('cf-workers-preview-token')
+
+  if (!shouldTransform) {
+    // Just proxy the request
+    return fetch(url, req)
+  }
+
+  const linkHeader = req.headers.get('x-link')
+  const asyncSelector = req.headers.get('x-async')
+  const deferSelector = req.headers.get('x-defer')
+  const asyncHide = req.headers.get('x-no-async-hide') === 'true'
+  const shouldPush = req.headers.get('x-push') !== 'true'
+
+  const response = await getResponse(req, shouldPush);
+
+  // Add the link header
+  if (linkHeader) response.headers.append('Link', linkHeader)
+
+  // Fancy pantsy way of turning on/off features and applying them on an
+  // HTMLRewriter. Stored as [fnName, ...args][].
+  const commands = [
+    asyncHide && ['on' ,'head > style', new AsyncHideHandler()],
+    deferSelector && ['on', deferSelector, new AttrHandler('defer', true)],
+    asyncSelector && ['on', asyncSelector, new AttrHandler('async', true)],
+    ['transform', response],
+  ].filter(Boolean)
+
+  // Apply all the commands on the HTMLRewriter and send.
+  return commands.reduce((rewriter, [fnName, ...args]) => {
+    return rewriter[fnName](...args)
+  }, new HTMLRewriter())
+}
+
+class AsyncHideHandler {
+  text(text) {
+    text.replace(text.text.replace(/async-hide/, 'async-hide-noop'))
+  }
+}
+
+class AttrHandler {
+  constructor(attribute, value = true) {
+    this._attribute = attribute
+    this._value = value
+  }
+  element(element) {
+    element.setAttribute(this._attribute, this._value)
+  }
+}
